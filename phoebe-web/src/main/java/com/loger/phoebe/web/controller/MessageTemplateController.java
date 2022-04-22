06695e03599054d77eed9931fd686c694f4d6899
刘汉超 <1256982120@qq.com>
2022-04-22 13:14:57 +0800

    create

--- phoebe-web/src/main/java/com/loger/phoebe/web/controller/MessageTemplateController.java
+++ phoebe-web/src/main/java/com/loger/phoebe/web/controller/MessageTemplateController.java
@@ -1,13 +1,20 @@
 package com.loger.phoebe.web.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.loger.phoebe.common.enums.RespStatusEnum;
 import com.loger.phoebe.common.vo.BasicResultVO;
+import com.loger.phoebe.service.api.domain.MessageParam;
+import com.loger.phoebe.service.api.domain.SendRequest;
+import com.loger.phoebe.service.api.domain.SendResponse;
+import com.loger.phoebe.service.api.enums.BusinessCode;
+import com.loger.phoebe.service.api.service.SendService;
 import com.loger.phoebe.support.domain.MessageTemplate;
 import com.loger.phoebe.web.service.MessageTemplateService;
+import com.loger.phoebe.web.vo.MessageTemplateParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 /**
  * @author chao
@@ -18,14 +25,34 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/messageTemplate")
+// @CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true", allowedHeaders = "*")
 public class MessageTemplateController {
 
     @Autowired
     private MessageTemplateService messageTemplateService;
 
+    @Autowired
+    private SendService sendService;
+
     @PostMapping("/save")
     public BasicResultVO saveOfUpdate(@RequestBody MessageTemplate messageTemplate){
         return BasicResultVO.success(messageTemplateService.saveOrUpdate(messageTemplate));
+    }
+
+    /**
+     * 测试发送接口
+     */
+    @PostMapping("test")
+    public BasicResultVO test(@RequestBody MessageTemplateParam messageTemplateParam) {
+
+        Map<String, String> variables = JSON.parseObject(messageTemplateParam.getMsgContent(), Map.class);
+        MessageParam messageParam = MessageParam.builder().receiver(messageTemplateParam.getReceiver()).variables(variables).build();
+        SendRequest sendRequest = SendRequest.builder().code(BusinessCode.COMMON_SEND.getCode()).messageTemplateId(messageTemplateParam.getId()).messageParam(messageParam).build();
+        SendResponse response = sendService.send(sendRequest);
+        if (response.getCode() != RespStatusEnum.SUCCESS.getCode()) {
+            return BasicResultVO.fail(response.getMsg());
+        }
+        return BasicResultVO.success(response);
     }
 
 
