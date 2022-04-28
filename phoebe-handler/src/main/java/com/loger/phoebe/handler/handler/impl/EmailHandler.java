32ca6d910369c41382c43fc9df97d2061356ba46
刘汉超 <1256982120@qq.com>
2022-04-28 11:26:09 +0800

    将springboot添加到nacos 实现动态配置

--- phoebe-handler/src/main/java/com/loger/phoebe/handler/handler/impl/EmailHandler.java
+++ phoebe-handler/src/main/java/com/loger/phoebe/handler/handler/impl/EmailHandler.java
@@ -1,9 +1,15 @@
 package com.loger.phoebe.handler.handler.impl;
 
+import cn.hutool.extra.mail.MailAccount;
+import cn.hutool.extra.mail.MailUtil;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.google.common.base.Throwables;
 import com.loger.phoebe.common.domain.TaskInfo;
+import com.loger.phoebe.common.dto.model.EmailContentModel;
 import com.loger.phoebe.common.enums.ChannelType;
 import com.loger.phoebe.handler.handler.BaseHandler;
 import com.loger.phoebe.handler.handler.Handler;
+import com.sun.mail.util.MailSSLSocketFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -18,17 +24,63 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailHandler extends BaseHandler implements Handler {
 
+    @NacosValue(value = "${email.host}", autoRefreshed = true)
+    private String host;
+    @NacosValue(value = "${email.port}", autoRefreshed = true)
+    private Integer port;
+    @NacosValue(value = "${email.auth}", autoRefreshed = true)
+    private Boolean auth;
+    @NacosValue(value = "${email.from}", autoRefreshed = true)
+    private String from;
+    @NacosValue(value = "${email.user}", autoRefreshed = true)
+    private String user;
+    @NacosValue(value = "${email.pass}", autoRefreshed = true)
+    private String pass;
+    @NacosValue(value = "${email.timeout}", autoRefreshed = true)
+    private Integer timeout;
+    @NacosValue(value = "${email.connection-timeout}", autoRefreshed = true)
+    private Integer connectionTimeout;
 
     public EmailHandler(){
         channelCode = ChannelType.EMAIL.getCode();
     }
 
-
     @Override
     public boolean handler(TaskInfo taskInfo) {
-        log.info("发送 emial : {}", taskInfo);
-        return false;
+        EmailContentModel emailContentModel = (EmailContentModel) taskInfo.getContentModel();
+        MailAccount account = getAccountConfig(taskInfo.getSendAccount());
+        try {
+            MailUtil.send(account, taskInfo.getReceiver(), emailContentModel.getTitle(),
+                    emailContentModel.getContent(), true, null);
+        } catch (Exception e) {
+            log.error("EmailHandler#handler fail!{},params:{}", Throwables.getStackTraceAsString(e), taskInfo);
+            return false;
+        }
+        return true;
     }
+
+    private MailAccount getAccountConfig(Integer sendAccount) {
+        MailAccount account = new MailAccount();
+        account.setHost(host);
+        account.setPort(port);
+        account.setAuth(auth);
+        account.setFrom(from);
+        account.setUser(user);
+        // 邮箱授权码
+        account.setPass(pass);
+
+        try {
+            MailSSLSocketFactory sf = new MailSSLSocketFactory();
+            sf.setTrustAllHosts(true);
+            account.setAuth(true).setStarttlsEnable(true).setSslEnable(true).setCustomProperty("mail.smtp.ssl.socketFactory", sf);
+            account.setTimeout(timeout).setConnectionTimeout(connectionTimeout);
+        } catch (Exception e) {
+            log.error("EmailHandler#getAccount fail!{}", Throwables.getStackTraceAsString(e));
+        }
+
+        return account;
+    }
+
 
 
 }
--- phoebe-web/src/main/java/com/loger/phoebe/PhoebeApplication.java
+++ phoebe-web/src/main/java/com/loger/phoebe/PhoebeApplication.java
@@ -1,8 +1,15 @@
 package com.loger.phoebe;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author chao
@@ -13,8 +20,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan(basePackages = {"com.loger.phoebe.support.dao"})
+@NacosPropertySource(dataId = "platform_config.yaml", autoRefreshed = true)
 public class PhoebeApplication {
+
+    @NacosInjected
+    private NamingService namingService;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @Value("${server.port}")
+    private Integer serverPort;
+
+
     public static void main(String[] args) {
         SpringApplication.run(PhoebeApplication.class, args);
     }
+
+    /**
+     * 服务注册到 Nacos
+     * @throws NacosException
+     */
+    @PostConstruct
+    public void registerService() throws NacosException {
+        namingService.registerInstance(applicationName, "127.0.0.1", serverPort);
+    }
+
+
 }
